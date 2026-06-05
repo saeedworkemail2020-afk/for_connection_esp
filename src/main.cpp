840a1e2e8c1ac6280693abd12dd0affc7816a1fb
Saeed <saeedworkemail2020@gmail.com>
2026-06-05 19:28:37 +0400

    Test: exchange data

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,53 @@
-#include <Arduino.h>
-#include <esp32-hal-gpio.h>
+#include <WiFi.h>
+#include <WebSocketsServer.h>
 
-void setup()
-{
-    pinMode(12, OUTPUT);
+const char* ssid = "ESP32_AP";
+const char* password = "12345678";
+
+WebSocketsServer webSocket = WebSocketsServer(81);
+
+int ledPin = 12;
+int counter = 0;
+
+void onWebSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+  
+  // counter++;
+  //     String text = "Message " + String(counter);
+  //     webSocket.sendTXT(num, text);
+  Serial.println(num);
+  if (type == WStype_TEXT) {
+    String msg = String((char*)payload);
+
+    if (msg == "on") {
+      digitalWrite(ledPin, HIGH);
+    }
+
+    if (msg == "off") {
+      digitalWrite(ledPin, LOW);
+    }
+  }
+}
+void count(){
+
+    counter++;
+      String text = "Message " + String(counter);
+      webSocket.sendTXT(0, text);
+}
+void setup() {
+  Serial.begin(921600);
+
+  pinMode(ledPin, OUTPUT);
+
+  WiFi.softAP(ssid, password);
+
+  Serial.println("AP Started");
+  Serial.println(WiFi.softAPIP());
+
+  webSocket.begin();
+  webSocket.onEvent(onWebSocketEvent);
 }
 
-void loop()
-{
-
-
-
+void loop() {
+  webSocket.loop();
+  count();
 }
