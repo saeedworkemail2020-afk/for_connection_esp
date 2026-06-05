0c53c36a17bc5d61b4bd8cc990b5ff2b3ae7d9f7
Saeed <saeedworkemail2020@gmail.com>
2026-06-05 17:07:48 +0400

    Edit(main):clear main

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,14 @@
 #include <Arduino.h>
+#include <esp32-hal-gpio.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+    pinMode(12, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+
 }
