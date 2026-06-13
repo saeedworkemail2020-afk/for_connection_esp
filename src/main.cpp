ba5a060aa22f553ee70dfdc03cb9e7f24cf8c365
Saeed <saeedworkemail2020@gmail.com>
2026-06-13 19:52:08 +0400

    Feature:tuch added

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,10 @@ WebServer server(80);
 
 int ledPin = 12;
 int sensorNumber = 0;
+int tuched=0;
+bool waitingForT4 = false;
+unsigned long touchTime = 0;
+
 void handleData() {
   String json = "{";
   json += "\"number\":" + String(sensorNumber);
@@ -38,8 +42,11 @@ void onWebSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t leng
           digitalWrite(ledPin, LOW);
            status="off";
       }
-        webSocket.sendTXT(num, status);
-      
+      String json =
+"{\"status\":\"" + status +
+"\",\"touch\":" + String(tuched) +
+"}";
+webSocket.sendTXT(num, json);      
     }
 
   }
@@ -64,7 +71,30 @@ void setup() {
 }
 
 void loop() {
+
   webSocket.loop();
-    server.handleClient();
-sensorNumber++;
+  server.handleClient();
+
+  sensorNumber++;
+
+  if (!waitingForT4 && touchRead(T3) < 40) {
+    waitingForT4 = true;
+    touchTime = millis();
+  }
+
+  if (waitingForT4 && millis() - touchTime >= 1000) {
+
+    if (touchRead(T4) > 40) {
+      tuched++;
+
+      String json =
+        "{\"status\":\"" + status +
+        "\",\"touch\":" + String(tuched) +
+        "}";
+
+      webSocket.broadcastTXT(json); // بهتر از sendTXT
+    }
+
+    waitingForT4 = false;
+  }
 }
