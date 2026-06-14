99f3dd9d110e5761225a20c2a33b55ad8b2e7bf7
Saeed <saeedworkemail2020@gmail.com>
2026-06-14 10:53:24 +0400

    Clean:remove msg

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,6 @@ void onWebSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t leng
   String cmd = doc["cmd"];
 
   if (type == WStype_TEXT) {
-    String msg = String((char*)payload);
 
     if (cmd == "led") {
       if (status== "off")
